cf0333c753cbee6b4a480c699decdfe0cb259153
Francisco-Pages <fj_pages@hotmail.com>
2022-09-29 12:05:13 -0400

    remove wao_zip

--- wao_zip/author_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthorAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'author_app'
-
-    def ready(self):
-        import author_app.signals
--- wao_zip/story_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StoryAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'story_app'
-
-    def ready(self):
-        import story_app.signals
